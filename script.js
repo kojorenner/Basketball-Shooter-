a997c071b6533753d9004501b1d8b5ee3c5fa5f6
Kojo Opoku-Renner <KojoRennerSchool@gmail.com>
2026-09-12 20:58:07 -0400

    Add court lines to canvas background

--- script.js
+++ script.js
@@ -87,8 +87,22 @@ function resetBall() {
   ball.vy = 0;
 }
 
+function drawCourt() {
+  ctx.strokeStyle = "#555";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(0, 250);
+  ctx.lineTo(500, 250);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(250, 250, 50, 0, Math.PI * 2);
+  ctx.stroke();
+}
+
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCourt();
   drawHoop();
   update();
   drawBall();
